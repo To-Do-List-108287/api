8e812b5d36d9cd1710168c7cb3ebc60e0b745eb1
Migas77 <miguel.belchior@ua.pt>
2024-11-04 18:59:30 +0000

    SCRUM-14 | feat: update task (check if there are any non-null values first)

--- src/main/java/com/_108287/api/service/TaskService.java
+++ src/main/java/com/_108287/api/service/TaskService.java
@@ -2,11 +2,15 @@ package com._108287.api.service;
 
 import com._108287.api.dto.CreateRequestTaskDTO;
 import com._108287.api.dto.ResponseTaskDTO;
+import com._108287.api.dto.UpdateRequestTaskDTO;
+
 import java.util.Optional;
 
 public interface TaskService {
 
   Optional<ResponseTaskDTO> createTask(CreateRequestTaskDTO createRequestTaskDTO, String sub);
   Optional<Long> deleteTask(Long id, String sub);
+
+  Optional<ResponseTaskDTO> updateTask(Long id, UpdateRequestTaskDTO updateRequestTaskDTO, String sub);
 
 }
